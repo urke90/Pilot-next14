5ff4689ccbd70032c5aa0a6340db0b1aa21fa62d
urke90 <urosbijelic90@gmail.com>
2024-02-28 11:47:12 +0100

    added colors from figma

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
-import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 
-export default function Home() {
-  return <h1>This is init commit</h1>;
-}
+// 1. izvaditi sve boje iz figme u tailwind config
+// 2. namestiti font-family
+// 3. namestiti sve variants button-a
+// * kada namestim config i button as example
+// 1. MongoDB --- nov projekat
+// 2. mongoose --- namestiti povezati
+// 3. Next-auth --- email pw, google, github
+// 4. Login page --- sign-up i sign-in ( prvo uraditi sa github-om pa google)
+
+// * branch
+// feature/tailwind-styles
+// feat / tailwind - styling;
+// feat / authentication;
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Button>dsadasdasdsa</Button>
+    </div>
+  );
+};
+
+export default Home;
