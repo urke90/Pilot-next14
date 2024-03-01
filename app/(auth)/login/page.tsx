ba49d6ba151b227f8a1e9dc08318c9c1844f056f
urke90 <urosbijelic90@gmail.com>
2024-03-01 10:54:32 +0100

    init setup of next-auth, added github as provider, created SessionProvider component and implmented in RootLayout

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,18 @@
+
+/**
+ * 1. add github login
+ * 2. add google login
+ * 3. add credentials login
+ */
+
+// ----------------------------------------------------------------
+
+const Login = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Login
